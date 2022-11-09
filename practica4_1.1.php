66081279a22d1c0810fc471c4a0fb5129996040f
admin <javiervg01@gmail.com>
2022-11-09 17:40:51 +0100

    v bueno, no me quiero morir al menos

--- practica4_1.1.php
+++ practica4_1.1.php
@@ -1,8 +1,39 @@
 <?php
     if (isset($_POST['enviar'])) { //se comprueba que ya está definido el formulario por su nombre
         $c1 = $_POST['campo1'];
-        $c2 = $_POST['campo2'];
-        $c3 = $_POST['campo3'];
-        echo $c1 + $c2;
+        $eur = $_POST['eur'];
+        $usd = $_POST['usd'];
+        $btc = $_POST['btc'];
+
+    if (!empty($_POST['eur'])) {
+
+        $eur = $_POST['eur'];
     }
-    ?>
+    if (!empty($_POST['usd'])) {
+
+        $usd = $_POST['usd'];
+    }
+    if (!empty($_POST['btc'])) {
+
+        $btc = $_POST['btc'];
+    }
+        //Valores redondeados y de 11/2022
+        $valorEur = 1;
+        $valorUsd = 1;
+        $valorBtc = 17036;
+        
+        if (isset($eur)) {
+            echo "El valor en USD es de: ", $c1 * $valorUsd;
+            echo "<br>";
+            echo "El valor en BTC es de: ", $c1 * $valorBtc;
+        } elseif (isset($usd)) {
+            echo "El valor en EUR es de: ", $c1 * $valorEur;
+            echo "<br>";
+            echo "El valor en BTC es de: ", $c1 * $valorBtc;
+            
+        } elseif (isset($btc)) {
+            echo "El valor en USD es de: ", $c1 * $valorUsd;
+            echo "<br>";
+            echo "El valor en EUR es de: ", $c1 * $valorEur;
+        }
+    }
--- practica4_1.php
+++ practica4_1.php
@@ -4,17 +4,28 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to c
 Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <form action="practica4_1.1.php"  method="post">
-            <input type="number" id="euro" name="campo1" placeholders="Inserta un dato"/>
-            <input type="number" id="dolar" name="campo2" placeholders="Inserta un dato"/>
-            <input type="number" id="btc" name="campo3" placeholders="Inserta un dato"/>
-            <input type="submit" value="Enviar" name="enviar"/> <!-- se agrega nombre -->
-        </form>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Javier Villacorta">
+    <title></title>
+</head>
+
+<body>
+    <form action="practica4_1.1.php" method="post">
+        <input type="number" id="cantidad" name="campo1" placeholders="Inserta un dato" />
+        <br>
+
+        <input type="radio" id="eur" name="currency" value="eur">
+        <label for="eur">Euro</label><br>
+        <input type="radio" id="usd" name="currency" value="usd">
+        <label for="usd">Dolar</label><br>
+        <input type="radio" id="btc" name="currency" value="btc">
+        <label for="btc">Bitcoin</label>
+        <br>
+
+        <input type="submit" value="Enviar" name="enviar" />
+    </form>
+</body>
 
 </html>
