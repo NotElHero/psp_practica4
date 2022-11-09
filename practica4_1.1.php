4ca2825d908671646b8d56595b170a0377dc2daa
admin <javiervg01@gmail.com>
2022-11-09 19:00:07 +0100

    v aaaaaa

--- practica4_1.1.php
+++ practica4_1.1.php
@@ -1,37 +1,22 @@
 <?php
-    if (isset($_POST['enviar'])) { //se comprueba que ya está definido el formulario por su nombre
+    if (isset($_POST['enviar'])) {
         $c1 = $_POST['campo1'];
-        $eur = $_POST['eur'];
-        $usd = $_POST['usd'];
-        $btc = $_POST['btc'];
+        $currency = $_POST['currency'];
 
-    if (!empty($_POST['eur'])) {
-
-        $eur = $_POST['eur'];
-    }
-    if (!empty($_POST['usd'])) {
-
-        $usd = $_POST['usd'];
-    }
-    if (!empty($_POST['btc'])) {
-
-        $btc = $_POST['btc'];
-    }
         //Valores redondeados y de 11/2022
         $valorEur = 1;
         $valorUsd = 1;
         $valorBtc = 17036;
         
-        if (isset($eur)) {
+        if (isset($currency['eur'])) {
             echo "El valor en USD es de: ", $c1 * $valorUsd;
             echo "<br>";
             echo "El valor en BTC es de: ", $c1 * $valorBtc;
-        } elseif (isset($usd)) {
+        }elseif (isset($currency['usd'])) {
             echo "El valor en EUR es de: ", $c1 * $valorEur;
             echo "<br>";
             echo "El valor en BTC es de: ", $c1 * $valorBtc;
-            
-        } elseif (isset($btc)) {
+        }elseif (isset($currency['btc'])) {
             echo "El valor en USD es de: ", $c1 * $valorUsd;
             echo "<br>";
             echo "El valor en EUR es de: ", $c1 * $valorEur;
